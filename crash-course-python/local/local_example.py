b56ea30f2b1bd708e2e92460e98407f6779b1ea4
James Tacker <james.tacker@saucelabs.com>
2020-02-18 15:04:23 -0800

    updated python ChromeOptions()

--- crash-course-python/local/local_example.py
+++ crash-course-python/local/local_example.py
@@ -4,9 +4,17 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from selenium import webdriver
 
-os_version = 'mac64'
-driver_version = 'chromedriver80'
-driver = webdriver.Chrome('./webdrivers/' + os_version + '/' + driver_version + '/chromedriver')
+# For Local Runs #
+# os_version = 'mac64'
+# driver_version = 'chromedriver80'
+# driver = webdriver.Chrome('./webdrivers/' + os_version + '/' + driver_version + '/chromedriver')
+
+# For Remote Runs i.e. Gitpod ##
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', chrome_options=options )
+
 driver.get("https://www.saucedemo.com")
 
 driver.find_element_by_id('user-name').send_keys('standard_user')
