36e5884323e5093ade4056a337e6a21d12254cd1
James Tacker <james.tacker@saucelabs.com>
2020-02-18 13:53:22 -0800

    updated dockerfile

--- crash-course-ruby/local/local_example.rb
+++ crash-course-ruby/local/local_example.rb
@@ -1,11 +1,18 @@
 require 'selenium-webdriver'
 
-os = "linux"
-driver_version="chromedriver80"
+## For Local Runs ##
+#os = "macOS"
+#driver_version="chromedriver80"
 
-Selenium::WebDriver::Chrome::Service.driver_path=('./webdrivers/' + os + '/' + driver_version + '/chromedriver')
+#Selenium::WebDriver::Chrome::Service.driver_path=('./webdrivers/' + os + '/' + driver_version + '/chromedriver')
+#@driver = Selenium::WebDriver.for :chrome
 
-@driver = Selenium::WebDriver.for :chrome
+## For Remote Runs i.e. Gitpod ##
+options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--no-sandbox')
+Selenium::WebDriver::Chrome::Service.driver_path=('/usr/bin/chromedriver')
+@driver = Selenium::WebDriver.for :chrome, options: options
+
 @driver.get "http://www.saucedemo.com/"
 
 @driver.find_element(id: 'user-name').send_keys('standard_user')
