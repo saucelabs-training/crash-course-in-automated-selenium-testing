db4558b41cf3742d10f615f6982f44ac38cfa5fa
James Tacker <james.tacker@saucelabs.com>
2020-02-19 14:24:42 -0800

    stripped answers

--- crash-course-ruby/local/local_example.rb
+++ crash-course-ruby/local/local_example.rb
@@ -1,4 +1,4 @@
-require 'selenium-webdriver'
+#require 'selenium-webdriver'
 
 ## For Local Runs ##
 #os = "macOS"
@@ -8,18 +8,18 @@ require 'selenium-webdriver'
 #@driver = Selenium::WebDriver.for :chrome
 
 ## For Remote Runs i.e. Gitpod ##
-options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-Selenium::WebDriver::Chrome::Service.driver_path=('/usr/bin/chromedriver')
-@driver = Selenium::WebDriver.for :chrome, options: options
+#options = Selenium::WebDriver::Chrome::Options.new
+#    options.add_argument('--no-sandbox')
+#    options.add_argument('--disable-dev-shm-usage')
+#Selenium::WebDriver::Chrome::Service.driver_path=('/usr/bin/chromedriver')
+#@driver = Selenium::WebDriver.for :chrome, options: options
+#
+#@driver.get "http://www.saucedemo.com/"
 
-@driver.get "http://www.saucedemo.com/"
 
-@driver.find_element(id: 'user-name').send_keys('standard_user')
-@driver.find_element(id: 'password').send_keys('secret_sauce')
-@driver.find_element(css: "[type='submit']").click
 
-actual_result = @driver.current_url == "https://www.saucedemo.com/inventory.html" ? "passed" : "failed"
-puts "The test #{actual_result}"
+
+
+
+
 @driver.quit
