b5380b358d747d4370c387573b8695688b2efb94
James Tacker <james.tacker@saucelabs.com>
2020-02-26 14:29:58 -0800

    updated local/local_example.py

--- crash-course-python/local/local_example.py
+++ crash-course-python/local/local_example.py
@@ -1,26 +1,26 @@
-# Selenium 3.14+ doesn't enable certificate checking
-import os
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-from selenium import webdriver
+# # Selenium 3.14+ doesn't enable certificate checking
+# import os
+# import urllib3
+# urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+# from selenium import webdriver
+#
+# # For Local Runs #
+# # os_version = 'mac64'
+# # driver_version = 'chromedriver80'
+# # driver = webdriver.Chrome('./webdrivers/' + os_version + '/' + driver_version + '/chromedriver')
+#
+# # For Remote Runs i.e. Gitpod ##
+# options = webdriver.ChromeOptions()
+# options.add_argument('--no-sandbox')
+# options.add_argument('--disable-dev-shm-usage')
+# driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', chrome_options=options )
+#
+# driver.get("https://www.saucedemo.com")
 
-# For Local Runs #
-# os_version = 'mac64'
-# driver_version = 'chromedriver80'
-# driver = webdriver.Chrome('./webdrivers/' + os_version + '/' + driver_version + '/chromedriver')
 
-# For Remote Runs i.e. Gitpod ##
-options = webdriver.ChromeOptions()
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', chrome_options=options )
 
-driver.get("https://www.saucedemo.com")
 
-driver.find_element_by_id('user-name').send_keys('standard_user')
-driver.find_element_by_id('password').send_keys('secret_sauce')
-driver.find_element_by_css_selector("[type='submit']").click()
 
-actual_result = "passed" if driver.current_url == "https://www.saucedemo.com/inventory.html" else "failed"
-print('The test ' + actual_result)
-driver.quit()
+
+
+# driver.quit()
