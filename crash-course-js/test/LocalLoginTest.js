b2db693cf52dd0c8135a596289b53697afc73a1c
James Tacker <james.tacker@saucelabs.com>
2020-06-16 10:54:32 -0700

    updated for gitpod

--- crash-course-js/test/LocalLoginTest.js
+++ crash-course-js/test/LocalLoginTest.js
@@ -6,9 +6,19 @@ describe('Login', function() {
     let driver
 
     beforeEach(async function() {
+        // For Local Runs
         const vendorDirectory = path.delimiter + path.join(__dirname, '..', 'vendor')
         process.env.PATH += vendorDirectory
         driver = await new Builder().forBrowser('chrome').build()
+
+        // For GitPod Runs
+        // let opts = new chrome.Options();
+        // opts.addArguments("--no-sandbox");
+        // opts.addArguments("--disable-dev-shm-usage");
+        // driver = await new Builder()
+        //     .forBrowser('chrome')
+        //     .setChromeOptions(opts)
+        //     .build();
     })
 
     afterEach(async function() {
